7218a6684129cf84ab83418e1b70d89d45cd4034
Greg.Wallerus <gwallerus@gmail.com>
2018-01-24 22:55:26 -0600

    Arraylist Added, One more step Left

--- Car.java
+++ Car.java
@@ -1,7 +1,20 @@
 
-public class Car {
+import java.util.ArrayList;
+
+public class Car  {
 
 	public void run() {
+	
+		ArrayList<Object> parts = new ArrayList<Object>();
+		
+		CarPart a = new Engine();
+		CarPart b = new FuelTank();
+		CarPart c = new Wheels();
+		
+		parts.add(a);
+		parts.add(b);
+		parts.add(c);
+		
 		
 	}
 	
